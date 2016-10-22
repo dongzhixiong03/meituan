db7028a0b1c1f637ab68f4f3b06c473bcb8c71b7
dongzhixiong10 <dongzhixiong10@163.com>
2016-10-22 17:42:29 +0800

    新建一个Person类

---
+++ gitmeituan/gitmeituan/Person.h
@@ -0,0 +1,14 @@
+//
+//  Person.h
+//  gitmeituan
+//
+//  Created by 时尚致爱 on 16/10/22.
+//  Copyright © 2016年 shishang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+@property (nonatomic,assign)int age;
+
+@end
---
+++ gitmeituan/gitmeituan/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  gitmeituan
+//
+//  Created by 时尚致爱 on 16/10/22.
+//  Copyright © 2016年 shishang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
